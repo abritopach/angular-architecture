9cafad5971e8e8b85ab7de4ce65b14e4dd7aaf4e
abritopach <adrbrpa1988@gmail.com>
2019-02-12 12:47:58 +0100

    Added heroes Virtual scrolling

--- src/ui/material/material.module.ts
+++ src/ui/material/material.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 
 // Angular Material Components.
 import { MatListModule, MatLineModule, MatToolbarModule, MatSidenavModule, MatSlideToggleModule, MatCardModule,
-         MatButtonModule, MatIconModule, MatIcon } from '@angular/material';
+         MatButtonModule, MatIconModule } from '@angular/material';
+
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @NgModule({
   imports: [MatListModule, MatLineModule, MatToolbarModule, MatSidenavModule, MatSlideToggleModule, MatCardModule, MatButtonModule,
-            MatIconModule],
+            MatIconModule, ScrollingModule],
   exports: [MatListModule, MatLineModule, MatToolbarModule, MatSidenavModule, MatSlideToggleModule, MatCardModule, MatButtonModule,
-            MatIconModule]
+            MatIconModule, ScrollingModule]
 })
 export class MyCustomMaterialModule {}
