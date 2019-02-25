e54b2134d97edffc110c245cbd24830c36d886a6
abritopach <adrbrpa1988@gmail.com>
2019-02-25 12:38:46 +0100

    Updated login styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MyCustomMaterialModule } from './ui/material/material.module';
-import { FooterComponent } from './common/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- src/app/common/shared-components.module.ts
+++ src/app/common/shared-components.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { MyCustomMaterialModule } from '../ui/material/material.module';
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [
-CommonModule,
+    CommonModule,
     MyCustomMaterialModule
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, FooterComponent]
 })
 export class SharedComponentsModule { }
